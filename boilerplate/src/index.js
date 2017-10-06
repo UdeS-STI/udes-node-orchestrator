5df63936a4389c737d6efcdc7f6243a106b94f63
Jessy Lachapelle <jessy.lachapelle@usherbrooke.ca>
2017-10-06 12:50:45 -0400

    Update boilerplate

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,6 +1,21 @@
+import Orchestrator, { ResponseHelper } from 'udes-node-orchestrator';
 import config from './config';
-import { getPostInfo } from './auth';
-import { Orchestrator } from '../../distribution/index';
+
+const getPostInfo = async (req, res) => {
+  const responseHelper = new ResponseHelper(req, res, config);
+  const { post } = responseHelper.getQueryParameters();
+  const options = {
+    method: 'GET',
+    url: `${config.apiUrl}/posts/${post}`,
+  };
+
+  try {
+    const data = await responseHelper.fetch(options);
+    responseHelper.handleResponse({ data });
+  } catch (err) {
+    responseHelper.handleError(err);
+  }
+};
 
 const routes = [
   { method: 'GET', url: '/post*', fn: getPostInfo },
