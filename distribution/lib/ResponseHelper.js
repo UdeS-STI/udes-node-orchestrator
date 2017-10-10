f9b72d94dc59223cfce1f289d9b6f5fe6ca436ea
Jessy Lachapelle <jessy.lachapelle@usherbrooke.ca>
2017-10-10 15:33:03 -0400

    Update distribution files

--- distribution/lib/ResponseHelper.js
+++ distribution/lib/ResponseHelper.js
@@ -28,6 +28,8 @@ var _RequestError2 = _interopRequireDefault(_RequestError);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -86,6 +88,21 @@ var getRange = exports.getRange = function getRange(req, query) {
   };
 };
 
+var getProxyTicket = function getProxyTicket(req, config) {
+  var renew = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+  return new Promise(function (resolve, reject) {
+    var targetService = config.cas.targetService;
+
+    req.getProxyTicket(targetService, { renew: renew }, function (err, pt) {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(pt);
+    });
+  });
+};
+
 /**
  * Handles response standardisation as well as http responses and requests.
  * @class
@@ -95,9 +112,225 @@ var getRange = exports.getRange = function getRange(req, query) {
  */
 
 var ResponseHelper = exports.ResponseHelper = function ResponseHelper(req, res, config) {
+  var _this = this;
+
   _classCallCheck(this, ResponseHelper);
 
-  _initialiseProps.call(this);
+  this.fetch = function (options) {
+    return new Promise(function () {
+      var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(resolve, reject) {
+        var body, _options$headers, headers, opt, time;
+
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                body = options.body, _options$headers = options.headers, headers = _options$headers === undefined ? getHeaders() : _options$headers;
+                opt = _extends({}, options, {
+                  auth: {
+                    user: _this.req.session.cas.user
+                  },
+                  body: body && (typeof body === 'undefined' ? 'undefined' : _typeof(body)) === 'object' ? JSON.stringify(body) : body,
+                  headers: headers
+                });
+                _context2.prev = 2;
+                _context2.next = 5;
+                return getProxyTicket(_this.req, _this.config);
+
+              case 5:
+                opt.auth.pass = _context2.sent;
+                time = +new Date();
+
+
+                (0, _request.request)(opt, function () {
+                  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(error, response) {
+                    var callDuration, meta;
+                    return regeneratorRuntime.wrap(function _callee$(_context) {
+                      while (1) {
+                        switch (_context.prev = _context.next) {
+                          case 0:
+                            callDuration = +new Date() - time;
+
+                            if (!error) {
+                              _context.next = 4;
+                              break;
+                            }
+
+                            reject(new _RequestError2.default(error, 500));
+                            return _context.abrupt('return');
+
+                          case 4:
+                            if (!(response.statusCode === 401)) {
+                              _context.next = 11;
+                              break;
+                            }
+
+                            _context.t0 = resolve;
+                            _context.next = 8;
+                            return getProxyTicket(_this.req, _this.config, true);
+
+                          case 8:
+                            _context.t1 = _context.sent;
+                            (0, _context.t0)(_context.t1);
+                            return _context.abrupt('return');
+
+                          case 11:
+                            meta = {
+                              count: response.headers[_this.config.customHeaderPrefix + '-count'] || 0,
+                              debug: {
+                                'x-TempsMs': callDuration
+                              },
+                              messages: response.headers[_this.config.customHeaderPrefix + '-messages'],
+                              status: response.statusCode
+                            };
+
+
+                            if (response.statusCode >= 200 && response.statusCode < 300) {
+                              try {
+                                resolve(_extends({}, JSON.parse(response.body), {
+                                  meta: meta
+                                }));
+                              } catch (err) {
+                                resolve({ data: response.body, meta: meta });
+                              }
+                            } else {
+                              reject(new _RequestError2.default(response.body || response, response.statusCode || 500));
+                            }
+
+                          case 13:
+                          case 'end':
+                            return _context.stop();
+                        }
+                      }
+                    }, _callee, _this);
+                  }));
+
+                  return function (_x5, _x6) {
+                    return _ref2.apply(this, arguments);
+                  };
+                }());
+                _context2.next = 14;
+                break;
+
+              case 10:
+                _context2.prev = 10;
+                _context2.t0 = _context2['catch'](2);
+
+                _this.req.log.error('Error when requesting PT, Authentication failed! ', _context2.t0);
+                reject(new _RequestError2.default(_context2.t0, 500));
+
+              case 14:
+              case 'end':
+                return _context2.stop();
+            }
+          }
+        }, _callee2, _this, [[2, 10]]);
+      }));
+
+      return function (_x3, _x4) {
+        return _ref.apply(this, arguments);
+      };
+    }());
+  };
+
+  this.getFile = function () {
+    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(options) {
+      var _options$headers2, headers, opt;
+
+      return regeneratorRuntime.wrap(function _callee3$(_context3) {
+        while (1) {
+          switch (_context3.prev = _context3.next) {
+            case 0:
+              _options$headers2 = options.headers, headers = _options$headers2 === undefined ? getHeaders() : _options$headers2;
+              opt = _extends({}, options, {
+                auth: {
+                  user: _this.req.session.cas.user
+                },
+                headers: headers
+              });
+              _context3.prev = 2;
+              _context3.next = 5;
+              return getProxyTicket(_this.req, _this.config);
+
+            case 5:
+              opt.auth.pass = _context3.sent;
+
+              Object.keys(headers).forEach(function (key) {
+                return _this.res.set(key, headers[key]);
+              });
+              _request.request.get(opt).pipe(_this.res);
+              _context3.next = 13;
+              break;
+
+            case 10:
+              _context3.prev = 10;
+              _context3.t0 = _context3['catch'](2);
+
+              _this.req.log.error('Error when requesting PT, Authentication failed! ', _context3.t0);
+
+            case 13:
+            case 'end':
+              return _context3.stop();
+          }
+        }
+      }, _callee3, _this, [[2, 10]]);
+    }));
+
+    return function (_x7) {
+      return _ref3.apply(this, arguments);
+    };
+  }();
+
+  this.getQueryParameters = function () {
+    return _url2.default.parse(_this.req.url, true).query;
+  };
+
+  this.getRequestParameters = function () {
+    return _extends({}, _this.getQueryParameters(), _this.req.body);
+  };
+
+  this.handleError = function (error) {
+    return _this.res.status(error.statusCode || 500).send(error.message || error);
+  };
+
+  this.handleResponse = function (data) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    var _options$formatData = options.formatData,
+        formatData = _options$formatData === undefined ? true : _options$formatData,
+        _options$headers3 = options.headers,
+        headers = _options$headers3 === undefined ? {} : _options$headers3;
+
+    var range = getRange(_this.req, _this.getQueryParameters());
+
+    Object.keys(headers).forEach(function (key) {
+      return _this.res.set(key, headers[key]);
+    });
+
+    if (range) {
+      var key = Object.keys(data)[0];
+      var values = Object.values(data)[0];
+      var size = values.length;
+      var start = range.start,
+          _range$end = range.end,
+          end = _range$end === undefined ? size - 1 : _range$end,
+          unit = range.unit;
+
+
+      if (start > end || start >= size || end >= size) {
+        _this.res.set('Content-Range', unit + ' */' + size);
+        return _this.handleError({ statusCode: 416, message: 'Cannot get range ' + start + '-' + end + ' of ' + size });
+      }
+
+      if (end - start !== size - 1) {
+        _this.res.set('Content-Range', unit + ' ' + start + '-' + end + '/' + size);
+        var partialData = _defineProperty({}, key, values.splice(start, end));
+        var responseData = formatData ? formatResponse(_this.req, partialData) : partialData;
+        return _this.res.status(206).send(responseData);
+      }
+    }
+
+    return _this.res.status(200).send(formatData ? formatResponse(_this.req, data) : data);
+  };
 
   this.req = req;
   this.res = res;
@@ -151,154 +384,3 @@ var ResponseHelper = exports.ResponseHelper = function ResponseHelper(req, res, 
  * @param {Boolean} [options.formatData=true] - True to standardise response format.
  */
 ;
-
-var _initialiseProps = function _initialiseProps() {
-  var _this = this;
-
-  this.fetch = function (options) {
-    return new Promise(function (resolve, reject) {
-      var body = options.body,
-          _options$headers = options.headers,
-          headers = _options$headers === undefined ? getHeaders() : _options$headers;
-
-      var opt = _extends({}, options, {
-        auth: {
-          user: _this.req.session.cas.user,
-          pass: _this.req.session.cas.pt
-        },
-        body: body && (typeof body === 'undefined' ? 'undefined' : _typeof(body)) === 'object' ? JSON.stringify(body) : body,
-        headers: headers
-      });
-
-      var getPT = function getPT(cb) {
-        var targetService = _this.config.cas.targetService;
-
-
-        _this.req.getProxyTicket(targetService, { renew: false }, function (err, pt) {
-          if (err) {
-            _this.req.log.error('Error when requesting PT, Authentication failed! ', err);
-            cb(err);
-          } else {
-            opt.auth.pass = pt;
-            var time = +new Date();
-
-            (0, _request.request)(opt, function (error, response) {
-              var callDuration = +new Date() - time;
-
-              if (error) {
-                return cb(error);
-              }
-
-              if (response.statusCode === 401) {
-                return _this.req.getProxyTicket(targetService, { renew: true }, cb);
-              }
-
-              var meta = {
-                count: response.headers[_this.config.customHeaderPrefix + '-count'] || 0,
-                debug: {
-                  'x-TempsMs': callDuration
-                },
-                messages: response.headers[_this.config.customHeaderPrefix + '-messages'],
-                status: response.statusCode
-              };
-
-              return cb(null, _extends({}, response, { meta: meta }));
-            });
-          }
-        });
-      };
-
-      getPT(function (err, res) {
-        if (err) {
-          reject(new _RequestError2.default(err, 500));
-        } else if (res.statusCode >= 200 && res.statusCode < 300) {
-          try {
-            resolve(_extends({}, JSON.parse(res.body), {
-              meta: res.meta
-            }));
-          } catch (error) {
-            resolve({ data: res.body, meta: res.meta });
-          }
-        } else {
-          reject(new _RequestError2.default(res.body || res, res.statusCode || 500));
-        }
-      });
-    });
-  };
-
-  this.getFile = function (options) {
-    var _options$headers2 = options.headers,
-        headers = _options$headers2 === undefined ? getHeaders() : _options$headers2;
-
-    var opt = _extends({}, options, {
-      auth: {
-        user: _this.req.session.cas.user,
-        pass: _this.req.session.cas.pt
-      },
-      headers: headers
-    });
-
-    _this.req.getProxyTicket(_this.config.cas.targetService, { renew: false }, function (err, pt) {
-      if (err) {
-        _this.req.log.error('Error when requesting PT, Authentication failed! ', err);
-      } else {
-        opt.auth.pass = pt;
-        Object.keys(headers).forEach(function (key) {
-          return _this.res.set(key, headers[key]);
-        });
-        _request.request.get(opt).pipe(_this.res);
-      }
-    });
-  };
-
-  this.getQueryParameters = function () {
-    return _url2.default.parse(_this.req.url, true).query;
-  };
-
-  this.getRequestParameters = function () {
-    return _extends({}, _this.getQueryParameters(), _this.req.body);
-  };
-
-  this.handleError = function (error) {
-    return _this.res.status(error.statusCode || 500).send(error.message || error);
-  };
-
-  this.handleResponse = function (data) {
-    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-    var _options$formatData = options.formatData,
-        formatData = _options$formatData === undefined ? true : _options$formatData,
-        _options$headers3 = options.headers,
-        headers = _options$headers3 === undefined ? {} : _options$headers3;
-
-    var range = getRange(_this.req, _this.getQueryParameters());
-
-    Object.keys(headers).forEach(function (key) {
-      return _this.res.set(key, headers[key]);
-    });
-
-    if (range) {
-      var key = Object.keys(data)[0];
-      var values = Object.values(data)[0];
-      var size = values.length;
-      var start = range.start,
-          _range$end = range.end,
-          end = _range$end === undefined ? size - 1 : _range$end,
-          unit = range.unit;
-
-
-      if (start > end || start >= size || end >= size) {
-        _this.res.set('Content-Range', unit + ' */' + size);
-        return _this.handleError({ statusCode: 416, message: 'Cannot get range ' + start + '-' + end + ' of ' + size });
-      }
-
-      if (end - start !== size - 1) {
-        _this.res.set('Content-Range', unit + ' ' + start + '-' + end + '/' + size);
-        var partialData = _defineProperty({}, key, values.splice(start, end));
-        var responseData = formatData ? formatResponse(_this.req, partialData) : partialData;
-        return _this.res.status(206).send(responseData);
-      }
-    }
-
-    return _this.res.status(200).send(formatData ? formatResponse(_this.req, data) : data);
-  };
-};
