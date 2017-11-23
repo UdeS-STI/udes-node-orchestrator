9c0110a9289142e2732104b7ddccbf4fe6eb52b7
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-11-23 08:17:22 -0500

    Export functions for auth info (#32)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import './jsDocs' // Import type definitions for jsDoc.
 import AuthHandler from './lib/AuthHandler'
+import { getAttributes, getUser } from './lib/auth'
 import Orchestrator from './server/Orchestrator'
 import ResponseFormatter from './lib/ResponseFormatter'
 import { ResponseHelper } from './lib/ResponseHelper'
 import Utils from './lib/Utils'
 
+const AuthUtils = {
+  getAttributes,
+  getUser,
+}
+
 export default Orchestrator
 export {
   AuthHandler,
+  AuthUtils,
   Orchestrator,
   ResponseHelper,
   ResponseFormatter,
